f48051256f4060e2bb6f9fda115165b3774d81cb
Siddharth Arya <63834479+siddhartharya0809@users.noreply.github.com>
2023-09-10 10:42:45 +0530

    added experience section

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Contact from '../components/Contact'
 import Main from '../components/Main'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
-
+import App from "../components/Experience.jsx"
 
 export default function Home() {
   return (
@@ -16,6 +16,7 @@ export default function Home() {
       </Head>
       <Main />
       <About />
+      <App />
       <Skills />
       <Projects />
       <Contact />
