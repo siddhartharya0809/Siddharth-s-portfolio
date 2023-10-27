126ea36e0cf7c9580d773b7faec3015776800c89
Siddharth Arya <63834479+siddhartharya0809@users.noreply.github.com>
2023-10-27 14:09:08 +0000

    added favicon icon

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+      <link rel="icon" type="image/x-icon" src="../public/favicon.ico" />
       <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
