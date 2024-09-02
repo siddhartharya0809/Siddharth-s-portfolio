2703c9021cc24d09e8334238ae319952059090e9
Siddharth Arya <63834479+siddhartharya0809@users.noreply.github.com>
2024-09-02 17:39:40 +0530

    nav bar bug solved

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,20 @@
-import Grid from "@/components/Grid";
+"use client";
+
+import { navItems } from "@/data";
+
 import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Client from "@/components/Client";
+import Experience from "@/components/Experience";
 import RecentProject from "@/components/RecentProject";
 import { FloatingNav } from "@/components/ui/FloatinNavbar";
-import Client from "@/components/Client";
-import { FaHome } from "react-icons/fa";
-import Experience from "@/components/Experience";
-import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 text-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="mzx-w-7xl w-full">
-        <FloatingNav
-          navItems={[
-            {
-              name: "Home",
-              link: "/",
-              icon: <FaHome />,
-            },
-          ]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProject />
