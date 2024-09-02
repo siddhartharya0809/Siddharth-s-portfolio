8f320517acb9249f93679600dbf78afca29cf0d9
Siddharth Arya <63834479+siddhartharya0809@users.noreply.github.com>
2024-09-02 17:42:53 +0530

    page visiblity bug resolved while scrolling up

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
+
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 import Client from "@/components/Client";
@@ -12,7 +13,7 @@ import { FloatingNav } from "@/components/ui/FloatinNavbar";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 text-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 text-white flex justify-center items-center flex-col  mx-auto sm:px-10 px-5">
       <div className="mzx-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
