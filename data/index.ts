22ab8b59041e083d774914f1b423c67a1904d56f
Siddharth Arya <63834479+siddhartharya0809@users.noreply.github.com>
2024-09-02 17:39:04 +0530

    added testimonials

--- data/index.ts
+++ data/index.ts
@@ -178,31 +178,17 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Research and Development Intern ",
+    desc: "Contributed to GIS-based mapping and data validation at CPR, enhancing UI/UX for an interactive data visualization project.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Fullstack Developer - Part Time",
+    desc: "Developed a responsive video podcast platform with live streaming and interactive features using React and Node.js.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
-  },
-  {
-    id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
-  },
-  {
-    id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
-    className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
   },
 ];
 
